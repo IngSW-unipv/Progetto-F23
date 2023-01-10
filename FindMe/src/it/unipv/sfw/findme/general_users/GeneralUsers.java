a4e49c739edb1ad70f33302ac5243de0aa5fece6
adda1703 <120007386+adda1703@users.noreply.github.com>
2023-01-10 16:13:46 +0100

    GeneralUsers

--- FindMe/src/it/unipv/sfw/findme/general_users/GeneralUsers.java
+++ FindMe/src/it/unipv/sfw/findme/general_users/GeneralUsers.java
@@ -1,5 +1,75 @@
 package it.unipv.sfw.findme.general_users;
 
 public abstract class GeneralUsers {
+	
+	protected String name;
+	protected String lastName;
+	protected String ID;
+	protected String email;
+	protected String password;
+	
+	
+	public GeneralUsers(String name, String lastName, String iD, String email, String password) {
+		super();
+		this.name = name;
+		this.lastName = lastName;
+		this.ID = iD;
+		this.email = email;
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getID() {
+		return ID;
+	}
+
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
 
 }

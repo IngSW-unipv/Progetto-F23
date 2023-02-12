040b61f39f5576e0b0e1d0f00340892168efb744
B077AS <118288280+SAMRDABOUR@users.noreply.github.com>
2023-02-12 11:17:01 +0100

    login

--- FindMe/src/it/unipv/sfw/findme/login/Login.java
+++ FindMe/src/it/unipv/sfw/findme/login/Login.java
@@ -1,63 +1,32 @@
 package it.unipv.sfw.findme.login;
 
 
-import java.io.*;
-import java.sql.*;
-import java.util.*;
-import it.unipv.sfw.findme.database.DBConnection;
-import it.unipv.sfw.findme.users.general_user.Users;
+import it.unipv.sfw.findme.users.general_user.UsersDAO;
+import it.unipv.sfw.findme.users.student.Students;
 
 public class Login {
+	String emailOrID;
+	String passwordString;
+	LoginGUI frame;
 
-	public Login (String emailOrID, char[] password, LoginGUI frame) {
-		String passwordString=new String(password);
-		int ID=0;
-		String type="";
-		String name="";
-		String lastName="";
-		String email="";
-		try {
+	public Login(String emailOrID, char[] password, LoginGUI frame) {
+			this.emailOrID=emailOrID;
+			this.passwordString=new String(password);
+			this.frame=frame;
 
-			Connection conn=DBConnection.connect();
-
-			String query="select * from GeneralUser where User_Code=? and Password=? or Email=? and Password=?";
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
-
-			preparedStmt.setString(1, emailOrID);
-			preparedStmt.setString(2, passwordString);
-			preparedStmt.setString(3, emailOrID);
-			preparedStmt.setString(4, passwordString);
-
-			ResultSet result=preparedStmt.executeQuery();
-
-			result.next();
-			ID=result.getInt(1);
-			type=result.getString(2);
-			name=result.getString(3);
-			lastName=result.getString(4);
-			email=result.getString(5);
-
-		} catch (Exception e1) {
-			//new ExceptionFrame("\u274C Wrong Email or Password");
-			return;
-		}
-
-
-		try {
-			Properties config= new Properties();
-			FileInputStream fis=new FileInputStream("Resources/Property/config.properties");
-			config.load(fis);
-			Users tempUser=selectType(name, lastName, Integer.toString(ID), email, passwordString, type, config);
-			tempUser.GUI(frame);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("errore lettura properties");
+	}	
+	
+	public void login() {
+		UsersDAO dao=new UsersDAO();
+		dao.selectUser(new Students(null, null, null, emailOrID, passwordString, null));
+		dao.getUser().GUI(frame);
+	}
+	
+	
+	public void fieldCheck() throws IllegalArgumentException{
+		if(emailOrID.isEmpty()==true || passwordString.isEmpty()==true) {
+			throw new IllegalArgumentException();
 		}
 	}
-
-	public Users selectType(String name, String lastName, String ID, String email, String password, String user, Properties property) throws Exception{
-		String userClassName=property.getProperty(user);
-		Users r=(Users)Class.forName(userClassName).getDeclaredConstructor(String.class, String.class, String.class, String.class, String.class).newInstance(name, lastName, ID, email, password);
-		return r;
-	}
+	
 }
